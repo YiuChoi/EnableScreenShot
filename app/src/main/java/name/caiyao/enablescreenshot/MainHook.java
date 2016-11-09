610ce1eba1d332dee8074b2a5d6abcf35891eaf6
蔡小木 <whucaiyao@gmail.com>
2016-11-09 10:37:27 +0800

    Initial commit

---
+++ app/src/main/java/name/caiyao/enablescreenshot/MainHook.java
@@ -0,0 +1,43 @@
+package name.caiyao.enablescreenshot;
+
+import android.os.Build;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by xiaomu on 2016/11/9 0009.
+ */
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        XposedHelpers.findAndHookMethod(Window.class, "setFlags", int.class, int.class,
+                mRemoveSecureFlagHook);
+        if (Build.VERSION.SDK_INT >= 17) {
+            XposedHelpers.findAndHookMethod(SurfaceView.class, "setSecure", boolean.class,
+                    mRemoveSetSecureHook);
+        }
+    }
+
+    private final XC_MethodHook mRemoveSecureFlagHook = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            Integer flags = (Integer) param.args[0];
+            flags &= ~WindowManager.LayoutParams.FLAG_SECURE;
+            param.args[0] = flags;
+        }
+    };
+
+    private final XC_MethodHook mRemoveSetSecureHook = new XC_MethodHook() {
+        @Override
+        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            param.args[0] = false;
+        }
+    };
+}
